533b7eda8487b62b048e698a51607392cc8b118d
Seungyoung <leesy960909@gmail.com>
2026-02-02 10:46:36 +0900

    chore: initial commit
    첫번째 커밋/환경 세팅

---
+++ src/main/java/sparta/cloudassignment/CloudAssignmentApplication.java
@@ -0,0 +1,13 @@
+package sparta.cloudassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAssignmentApplication.class, args);
+    }
+
+}
